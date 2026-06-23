d5e627d1280e63e65145a1ddd16b4acc04b639fb
aminu audu <aminuaudu61@gmail.com>
2026-06-23 10:42:03 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+this is color changer function
+document.addEventListener("DOMContentLoaded", function () {
+    
+    const colorBox = document.getElementById("color-box");
+    const button = document.getElementById("change-color-btn");
+
+    function getRandomColor() {
+        let letters = "0123456789ABCDEF";
+        let color = "#";
+
+        for (let i = 0; i < 6; i++) {
+            color += letters[Math.floor(Math.random() * 16)];
+        }
+
+        return color;
+    }
+
+    button.addEventListener("click", function () {
+        colorBox.style.backgroundColor = getRandomColor();
+    });
+
+});
